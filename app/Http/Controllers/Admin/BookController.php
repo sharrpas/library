3a79a953e12aa39c2363ec8ef831a6aba250f1df
Sina <sharrpas@gmail.com>
2022-05-20 13:15:01 +0430

    add book controller

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class BookController extends Controller
 {
@@ -17,9 +21,30 @@ class BookController extends Controller
 
     }
 
-    public function store()
+    public function store(Request $request)
     {
+        $validated_data = Validator::make($request->all(), [
+            'title' => 'required|min:4|max:30',
+            'author' => 'required|min:3|max:20',
+            'translator' => 'min:3|max:20',
+            'description' => 'min:4|max:500',
+//            'book' =>
+        ]);
+        if ($validated_data->fails())
+            return response()->json($validated_data->errors());
 
+        DB::beginTransaction();
+        $BookName = date('Ymdhis') . rand(100, 999) . '.pdf';
+        Storage::putFileAs('books', $request->file('book'), $BookName);
+
+        Book::query()->create([
+            'title' => $request->title,
+            'author' => $request->author,
+            'translator' => $request->translator,
+            'description' => $request->description,
+            'path' => $BookName,
+        ]);
+        DB::commit();
     }
 
     public function update()
