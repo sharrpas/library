b3883db10fe0d1038039d02fe7b553eacb1e09c4
Sina <sharrpas@gmail.com>
2022-05-31 20:24:51 +0430

    remove pagination

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -10,7 +10,7 @@ class BookController extends Controller
 {
     public function index()
     {
-        return Book::query()->paginate(10);
+        return response()->json(Book::all());
     }
 
 }
